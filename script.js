54c6359fed9f9039c6a198a6d5576823102d81b2
DavidHaney09 <davidhaney0787@gmail.com>
2026-01-19 18:08:57 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,51 +1,25 @@
-// ===== Carrusel =====
+function irA(url) {
+  window.location.href = url;
+}
+
+/* Carrusel automático */
 const imagenes = [
-  "https://picsum.photos/700/400?1",
-  "https://picsum.photos/700/400?2",
-  "https://picsum.photos/700/400?3",
-  "https://picsum.photos/700/400?4",
-  "https://picsum.photos/700/400?5"
+  "https://picsum.photos/800/400?random=1",
+  "https://picsum.photos/800/400?random=2",
+  "https://picsum.photos/800/400?random=3",
+  "https://picsum.photos/800/400?random=4"
 ];
 
-let indexImg = 0;
-const carrusel = document.getElementById("carrusel-img");
+let index = 0;
+const img = document.getElementById("carrusel-img");
 
 setInterval(() => {
-  indexImg = (indexImg + 1) % imagenes.length;
-  carrusel.src = imagenes[indexImg];
+  index = (index + 1) % imagenes.length;
+  img.style.opacity = 0;
+
+  setTimeout(() => {
+    img.src = imagenes[index];
+    img.style.opacity = 1;
+  }, 400);
+
 }, 3000);
-
-// ===== Música =====
-const canciones = [
-  "music/1.m4a",
-  "music/2.m4a",
-  "music/3.mp3",
-  "music/4.mp3",
-  "music/5.mp3"
-];
-
-const audio = document.getElementById("audio-player");
-let indexCancion = 0;
-
-audio.src = canciones[indexCancion];
-audio.volume = 1;
-
-// intenta autoplay
-window.addEventListener("load", () => {
-  audio.play().catch(() => {
-    document.body.addEventListener("click", () => {
-      audio.play();
-    }, { once: true });
-  });
-});
-
-audio.addEventListener("ended", () => {
-  indexCancion = (indexCancion + 1) % canciones.length;
-  audio.src = canciones[indexCancion];
-  audio.play();
-});
-
-// navegación
-function irA(pagina) {
-  window.location.href = pagina;
-}
